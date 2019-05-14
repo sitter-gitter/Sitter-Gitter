75841ee168d428802c8f9392241ee10f91cbae51
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-05-14 11:45:09 -0500

    initial files

---
+++ src/main/java/com/codeup/sittergitter/SittergitterApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.sittergitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SittergitterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SittergitterApplication.class, args);
+    }
+
+}
