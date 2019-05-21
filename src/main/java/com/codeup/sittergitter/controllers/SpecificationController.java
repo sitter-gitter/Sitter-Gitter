aec8002039de4cb1a33a543508c77dd9037d5123
mjtillman <mjtillman@gmail.com>
2019-05-21 08:55:55 -0500

    Working on specs form

--- src/main/java/com/codeup/sittergitter/controllers/SpecificationController.java
+++ src/main/java/com/codeup/sittergitter/controllers/SpecificationController.java
@@ -1,4 +1,29 @@
 package com.codeup.sittergitter.controllers;
 
+import com.codeup.sittergitter.models.Specification;
+import com.codeup.sittergitter.models.User;
+import com.codeup.sittergitter.repositories.SpecificationRepository;
+import com.codeup.sittergitter.repositories.UserRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
 public class SpecificationController {
+
+  private final UserRepository usersRepo;
+  private final SpecificationRepository specificationsRepo;
+
+  public SpecificationController(UserRepository usersRepo, SpecificationRepository specificationsRepo) {
+    this.usersRepo = usersRepo;
+    this.specificationsRepo = specificationsRepo;
+  }
+
+  @GetMapping("/users/{username}/edit/specifications")
+  public String showEditSpecs(@PathVariable String username, Model model) {
+    User user = usersRepo.findByUsername(username);
+    model.addAttribute("user", user);
+    return "users/edit-specifications";
+  }
 }
