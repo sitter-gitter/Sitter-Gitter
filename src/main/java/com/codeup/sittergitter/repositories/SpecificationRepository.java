4fcbee359912c6c00f592f802b93933698efefce
Erik Behnke <erik.behnke@gmail.com>
2019-05-21 21:57:56 -0500

    draft specs form and controller

--- src/main/java/com/codeup/sittergitter/repositories/SpecificationRepository.java
+++ src/main/java/com/codeup/sittergitter/repositories/SpecificationRepository.java
@@ -4,10 +4,14 @@ import com.codeup.sittergitter.models.Specification;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SpecificationRepository extends CrudRepository<Specification, Long> {
     //List<Specification> findAll();
 //    Specification findOne(Long id);
     //void save(Specification specification);
     //void delete(Specification specification);
+
+    List<Specification> findByBabysitterUsername(String username);
 }
