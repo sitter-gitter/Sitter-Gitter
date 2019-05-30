7ac44bec07f6a7d36f28c6f78b4a95de29cb91ff
Erik Behnke <erik.behnke@gmail.com>
2019-05-30 13:34:52 -0500

    changes to seeder and availablilty card

--- src/main/resources/seeder.sql
+++ src/main/resources/seeder.sql
@@ -3,74 +3,94 @@ USE sitter_gitter_db;
 
 INSERT INTO users (username, email, password, first_name, last_name, street_addr, city, state, zip_code,
                    is_babysitter)
-VALUES ('Dwight', 'dwight@codeup.com', '$2a$10$Vh/68GBeHSHiIgujx.zEzetpwORkBI0i7b9I43kyTIgP2JzTaz4Q2', 'Dwight', 'Bemisderfer', '600 Navarro St #350', 'San Antonio', 'TX', '78205', 0),
-       ('Erik', 'erik@codeup.com', '$2a$10$Vh/68GBeHSHiIgujx.zEzetpwORkBI0i7b9I43kyTIgP2JzTaz4Q2', 'Erik', 'Behnke', '600 Navarro St #350', 'San Antonio', 'TX', '78205', 0),
+VALUES ('Dwight', 'dwight@sitter-gitter.com', '$2a$10$Vh/68GBeHSHiIgujx.zEzetpwORkBI0i7b9I43kyTIgP2JzTaz4Q2', 'Dwight',
+        'Bemisderfer', '600 Navarro St #350', 'San Antonio', 'TX', '78205', 0),
+       ('Erik', 'erik@sitter-gitter.com', '$2a$10$Vh/68GBeHSHiIgujx.zEzetpwORkBI0i7b9I43kyTIgP2JzTaz4Q2', 'Erik',
+        'Behnke',
+        '600
+       Navarro St #350', 'San Antonio', 'TX', '78205', 0),
 
-       ('Mindy', 'mindy@codeup.com', '$2a$10$Vh/68GBeHSHiIgujx.zEzetpwORkBI0i7b9I43kyTIgP2JzTaz4Q2', 'Mindy',
+       ('Mindy', 'mindy@sitter-gitter.com', '$2a$10$Vh/68GBeHSHiIgujx.zEzetpwORkBI0i7b9I43kyTIgP2JzTaz4Q2', 'Mindy',
         'Tillman', '600 Navarro St #350', 'San Antonio', 'TX', '78205', 0),
 
-       ('Parent1', 'parent1@codeup.com', '$2a$10$Vh/68GBeHSHiIgujx.zEzetpwORkBI0i7b9I43kyTIgP2JzTaz4Q2', 'Parent1',
+       ('Daniel', 'daniel@sitter-gitter.com', '$2a$10$Vh/68GBeHSHiIgujx.zEzetpwORkBI0i7b9I43kyTIgP2JzTaz4Q2',
+        'Parent1',
         'Gitter', '600 Navarro St #1', 'San Antonio', 'TX', '78201', 0),
 
-       ('Parent2', 'parent2@codeup.com', '$2a$10$Vh/68GBeHSHiIgujx.zEzetpwORkBI0i7b9I43kyTIgP2JzTaz4Q2', 'Parent2',
+       ('Justin', 'justin@sitter-gitter.com', '$2a$10$Vh/68GBeHSHiIgujx.zEzetpwORkBI0i7b9I43kyTIgP2JzTaz4Q2',
+        'Parent2',
         'Gitter', '600 Navarro St #2', 'San Antonio', 'TX', '78202', 0),
 
-       ('Babysitter1', 'sitter1@codeup.com', '$2a$10$Vh/68GBeHSHiIgujx.zEzetpwORkBI0i7b9I43kyTIgP2JzTaz4Q2',
+       ('Alexis', 'alexis@sitter-gitter.com', '$2a$10$Vh/68GBeHSHiIgujx.zEzetpwORkBI0i7b9I43kyTIgP2JzTaz4Q2',
         'Baby1', 'Sitter', '600 Navarro St #1', 'San Antonio', 'TX', '78201', 1),
 
-       ('Babysitter2', 'sitter2@codeup.com', '$2a$10$Vh/68GBeHSHiIgujx.zEzetpwORkBI0i7b9I43kyTIgP2JzTaz4Q2',
+       ('Barbara', 'barbara@sitter-gitter.com', '$2a$10$Vh/68GBeHSHiIgujx.zEzetpwORkBI0i7b9I43kyTIgP2JzTaz4Q2',
         'Baby2', 'Sitter', '600 Navarro St #2', 'San Antonio', 'TX', '78202', 1),
 
-       ('Babysitter3', 'sitter3@codeup.com', '$2a$10$Vh/68GBeHSHiIgujx.zEzetpwORkBI0i7b9I43kyTIgP2JzTaz4Q2',
+       ('Bob', 'bob@sitter-gitter.com', '$2a$10$Vh/68GBeHSHiIgujx.zEzetpwORkBI0i7b9I43kyTIgP2JzTaz4Q2',
         'Baby3', 'Sitter', '600 Navarro St #3', 'San Antonio', 'TX', '78203', 1),
 
-       ('Babysitter4', 'sitter4@codeup.com', '$2a$10$Vh/68GBeHSHiIgujx.zEzetpwORkBI0i7b9I43kyTIgP2JzTaz4Q2',
+       ('Beatrice', 'beatrice@sitter-gitter.com', '$2a$10$Vh/68GBeHSHiIgujx.zEzetpwORkBI0i7b9I43kyTIgP2JzTaz4Q2',
         'Baby4', 'Sitter', '600 Navarro St #4', 'San Antonio', 'TX', '78204', 1),
 
-       ('Babysitter5', 'sitter5@codeup.com', '$2a$10$Vh/68GBeHSHiIgujx.zEzetpwORkBI0i7b9I43kyTIgP2JzTaz4Q2',
+       ('Brenda', 'brenda@sitter-gitter.com', '$2a$10$Vh/68GBeHSHiIgujx.zEzetpwORkBI0i7b9I43kyTIgP2JzTaz4Q2',
         'Baby5', 'Sitter', '600 Navarro St #5', 'San Antonio', 'TX', '78205', 1);
 
 
 INSERT INTO children (name, birthdate, special_note, parent_id)
 VALUES
-       ('Child1', '2015-12-11', 'Special Note', (SELECT id FROM users WHERE email = 'dwight@codeup.com')),
-       ('Child2', '2017-07-05', 'Child2 likes warm milk before going to bed.', (SELECT id FROM users WHERE email = 'dwight@codeup.com')),
-       ('Child3', '2018-03-09', 'Please give Child3 a bedtime story and a backrub to help her go to sleep.', (SELECT id FROM users WHERE email = 'dwight@codeup.com')),
-       ('Child4', '2015-12-11', 'Special Note', (SELECT id FROM users WHERE email = 'erik@codeup.com')),
-       ('Child5', '2017-07-05', 'Child5 likes warm milk before going to bed.', (SELECT id FROM users WHERE email =
+       ('Chris', '2015-12-11', 'Chris need to take a nap at 2pm.', (SELECT id FROM users WHERE email =
+                                                                                            'dwight@sitter-gitter
+       .com')),
+       ('Cory', '2017-07-05', 'Cory likes warm milk before going to bed.', (SELECT id FROM users WHERE email =
+                                                                                                           'dwight@sitter-gitter.com')),
+       ('Charlie', '2018-03-09', 'Please give Charlie a bedtime story and a backrub to help her go to sleep.', (SELECT
+                                                                                                                     id FROM users WHERE email = 'dwight@sitter-gitter.com')),
+       ('Clarise', '2015-12-11', 'Keep Clarise away from the cat food!', (SELECT id FROM users WHERE email =
+                                                                                              'erik@sitter-gitter.com')),
+       ('Cinthia', '2017-07-05', 'Please make sure Cinthia gets her Flinstone vitamins in the morning', (SELECT id FROM
+                                                                                                                   users WHERE
+                                                                                                                  email =
                                                                                                            'erik@codeup
                                                                                                            .com')),
-       ('Child6', '2018-03-09', 'Please give Child6 a bedtime story and a backrub to help her go to sleep.', (SELECT
-                                                                                                                     id FROM users WHERE email = 'erik@codeup.com')),
-       ('Child7', '2015-12-11', 'Special Note', (SELECT id FROM users WHERE email = 'mindy@codeup.com')),
-       ('Child8', '2017-07-05', 'Child8 likes warm milk before going to bed.', (SELECT id FROM users WHERE email =
+       ('Cindy', '2018-03-09', 'Please give Cindy a bedtime story and a backrub to help her go to sleep.', (SELECT
+                                                                                                                     id FROM users WHERE email = 'erik@sitter-gitter.com')),
+       ('Cleo', '2015-12-11', 'Keep Cleo away from the cat food!', (SELECT id FROM users WHERE email =
+                                                                                             'mindy@sitter-gitter.com')),
+       ('Carlo', '2017-07-05', 'Carlo likes warm milk before going to bed.', (SELECT id FROM users WHERE email =
                                                                                                            'mindy@codeup
                                                                                                            .com')),
-       ('Child9', '2018-03-09', 'Please give Child9 a bedtime story and a backrub to help her go to sleep.', (SELECT
-                                                                                                                      id FROM users WHERE email = 'mindy@codeup.com')),
-       ('Child10', '2015-12-11', 'Special Note', (SELECT id FROM users WHERE email = 'parent1@codeup.com')),
-       ('Child11', '2017-07-05', 'Child11 likes warm milk before going to bed.', (SELECT id FROM users WHERE email =
-                                                                                                           'parent1@codeup
+       ('Carly', '2018-03-09', 'Please give Carly a bedtime story and a backrub to help her go to sleep.', (SELECT
+                                                                                                                      id FROM users WHERE email = 'mindy@sitter-gitter.com')),
+       ('Charlotte', '2015-12-11', 'Please make sure Charlotte gets her Flinstone vitamins in the morning', (SELECT id
+       FROM users WHERE email = 'juston@sitter-gitter.com')),
+       ('Chance', '2017-07-05', 'Chance likes warm milk before going to bed.', (SELECT id FROM users WHERE email =
+                                                                                                           'daniel@codeup
                                                                                                            .com')),
-       ('Child12', '2018-03-09', 'Please give Child12 a bedtime story and a backrub to help her go to sleep.', (SELECT
-                                                                                                                     id FROM users WHERE email = 'parent1@codeup.com')),
-       ('Child13', '2015-12-11', 'Special Note', (SELECT id FROM users WHERE email = 'parent2@codeup.com')),
-       ('Child14', '2017-07-05', 'Child14 likes warm milk before going to bed.', (SELECT id FROM users WHERE email =
-                                                                                                           'parent2@codeup
+       ('Chuck', '2018-03-09', 'Please give Chuck a bedtime story and a backrub to help her go to sleep.', (SELECT
+                                                                                                                     id FROM users WHERE email = 'daniel@sitter-gitter.com')),
+       ('Charles', '2015-12-11', 'Keep Charles away from the cat food!', (SELECT id FROM users WHERE email =
+                                                                                               'justin@sitter-gitter
+                                                                                               .com')),
+       ('Cathy', '2017-07-05', 'Cathy likes warm milk before going to bed.', (SELECT id FROM users WHERE email =
+                                                                                                           'daniel@codeup
                                                                                                            .com')),
-       ('Child15', '2018-03-09', 'Please give Child15 a bedtime story and a backrub to help her go to sleep.', (SELECT
-                                                                                                                     id FROM users WHERE email = 'parent2@codeup.com')),
-       ('Child16', '2015-12-11', 'Special Note', (SELECT id FROM users WHERE email = 'erik@codeup.com')),
-       ('Child17', '2017-07-05', 'Child17 likes warm milk before going to bed.', (SELECT id FROM users WHERE email =
+       ('Cadence', '2018-03-09', 'Please give Cadence a bedtime story and a backrub to help her go to sleep.', (SELECT
+                                                                                                                     id FROM users WHERE email = 'justin@sitter-gitter.com')),
+       ('Caleb', '2015-12-11', 'Please make sure Caleb gets his Flinstone vitamins in the morning', (SELECT id FROM
+                                                                                                                    users
+       WHERE email = 'erik@sitter-gitter.com')),
+       ('Connor', '2017-07-05', 'Connor likes warm milk before going to bed.', (SELECT id FROM users WHERE email =
                                                                                                            'mindy@codeup
                                                                                                            .com')),
-       ('Child18', '2018-03-09', 'Please give Child18 a bedtime story and a backrub to help her go to sleep.', (SELECT
-                                                                                                                     id FROM users WHERE email = 'dwight@codeup.com'));
+       ('Chase', '2018-03-09', 'Please give Chase a bedtime story and a backrub to help her go to sleep.', (SELECT
+                                                                                                                     id FROM users WHERE email = 'dwight@sitter-gitter.com'));
 
 
 
 INSERT INTO available_times (start, end, babysitter_id, is_taken)
-VALUES ('2019-05-31 08:00:00', '2019-05-31 23:00:00', 6, 0),
+VALUES
+       ('2019-05-31 08:00:00', '2019-05-31 23:00:00', 6, 0),
        ('2019-06-01 08:00:00', '2019-06-01 23:00:00', 6, 0),
        ('2019-06-02 08:00:00', '2019-06-02 23:00:00', 6, 0),
        ('2019-06-03 08:00:00', '2019-06-03 23:00:00', 6, 0),
@@ -79,8 +99,6 @@ VALUES ('2019-05-31 08:00:00', '2019-05-31 23:00:00', 6, 0),
        ('2019-02-06 08:00:00', '2019-02-06 23:00:00', 6, 1),
        ('2019-04-07 08:00:00', '2019-04-07 21:00:00', 6, 1),
        ('2019-06-08 08:00:00', '2019-06-08 23:00:00', 6, 0),
-       ('2019-06-09 08:00:00', '2019-06-09 21:00:00', 6, 0),
-       ('2019-06-10 08:00:00', '2019-06-10 23:00:00', 6, 0),
        ('2019-05-31 08:00:00', '2019-05-31 23:00:00', 7, 0),
        ('2019-06-01 08:00:00', '2019-06-01 23:00:00', 7, 0),
        ('2019-06-02 08:00:00', '2019-06-02 23:00:00', 7, 0),
@@ -90,8 +108,6 @@ VALUES ('2019-05-31 08:00:00', '2019-05-31 23:00:00', 6, 0),
        ('2019-06-06 08:00:00', '2019-06-06 23:00:00', 7, 1),
        ('2019-06-07 08:00:00', '2019-06-07 21:00:00', 7, 1),
        ('2019-06-08 08:00:00', '2019-06-08 23:00:00', 7, 0),
-       ('2019-06-09 08:00:00', '2019-06-09 21:00:00', 7, 0),
-       ('2019-06-10 08:00:00', '2019-06-10 23:00:00', 7, 0),
        ('2019-05-31 08:00:00', '2019-05-31 23:00:00', 8, 0),
        ('2019-06-01 08:00:00', '2019-06-01 23:00:00', 8, 0),
        ('2019-06-02 08:00:00', '2019-06-02 23:00:00', 8, 0),
@@ -101,8 +117,6 @@ VALUES ('2019-05-31 08:00:00', '2019-05-31 23:00:00', 6, 0),
        ('2019-06-06 08:00:00', '2019-06-06 23:00:00', 8, 1),
        ('2019-06-07 08:00:00', '2019-06-07 21:00:00', 8, 1),
        ('2019-06-08 08:00:00', '2019-06-08 23:00:00', 8, 0),
-       ('2019-06-09 08:00:00', '2019-06-09 21:00:00', 8, 0),
-       ('2019-06-10 08:00:00', '2019-06-10 23:00:00', 8, 0),
        ('2019-05-31 08:00:00', '2019-05-31 23:00:00', 9, 0),
        ('2019-06-01 08:00:00', '2019-06-01 23:00:00', 9, 0),
        ('2019-06-02 08:00:00', '2019-06-02 23:00:00', 9, 0),
@@ -112,19 +126,15 @@ VALUES ('2019-05-31 08:00:00', '2019-05-31 23:00:00', 6, 0),
        ('2019-06-06 08:00:00', '2019-06-06 23:00:00', 9, 1),
        ('2019-06-07 08:00:00', '2019-06-07 21:00:00', 9, 1),
        ('2019-06-08 08:00:00', '2019-06-08 23:00:00', 9, 0),
-       ('2019-06-09 08:00:00', '2019-06-09 21:00:00', 9, 0),
-       ('2019-06-10 08:00:00', '2019-06-10 23:00:00', 9, 0),
        ('2019-05-31 08:00:00', '2019-05-31 23:00:00', 10, 0),
        ('2019-06-01 08:00:00', '2019-06-01 23:00:00', 10, 0),
        ('2019-06-02 08:00:00', '2019-06-02 23:00:00', 10, 0),
-       ('2019-06-03 08:00:00', '2019-06-03 23:00:00', 10, 0),
-       ('2019-06-04 08:00:00', '2019-06-04 23:00:00', 10, 0),
+       ('2019-06-03 08:00:00', '2019-06-03 23:00:00', 6, 0),
+       ('2019-06-04 08:00:00', '2019-06-04 23:00:00', 6, 0),
        ('2019-06-05 08:00:00', '2019-06-05 23:00:00', 10, 1),
        ('2019-06-06 08:00:00', '2019-06-06 23:00:00', 10, 1),
        ('2019-06-07 08:00:00', '2019-06-07 21:00:00', 10, 1),
-       ('2019-06-08 08:00:00', '2019-06-08 23:00:00', 10, 0),
-       ('2019-06-09 08:00:00', '2019-06-09 21:00:00', 10, 0),
-       ('2019-06-10 08:00:00', '2019-06-10 23:00:00', 10, 0);
+       ('2019-06-08 08:00:00', '2019-06-08 23:00:00', 10, 0);
 
 
 
@@ -138,37 +148,36 @@ VALUES ('2019-05-05 11:00:00', '2019-05-05 20:00:00', 1, 0, 6, 1, 6),
        ('2019-06-05 11:00:00', '2019-06-05 20:00:00', 1, 1, 8, 3, 28),
        ('2019-06-06 11:00:00', '2019-06-06 20:00:00', 1, 1, 8, 3, 29),
        ('2019-06-07 11:00:00', '2019-06-07 20:00:00', 1, 1, 8, 3, 30),
-       ('2019-06-05 11:00:00', '2019-06-05 20:00:00', 1, 1, 9, 4, 39),
-       ('2019-06-06 11:00:00', '2019-06-06 20:00:00', 1, 1, 9, 4, 40),
-       ('2019-06-07 11:00:00', '2019-06-07 20:00:00', 1, 0, 9, 4, 41),
-       ('2019-06-05 11:00:00', '2019-06-05 20:00:00', 1, 1, 10, 5, 51),
-       ('2019-06-06 11:00:00', '2019-06-06 20:00:00', 1, 1, 10, 5, 52),
-       ('2019-06-07 11:00:00', '2019-06-07 20:00:00', 1, 0, 10, 5, 53);
+       ('2019-06-05 11:00:00', '2019-06-05 20:00:00', 1, 1, 6, 1, 39),
+       ('2019-06-06 11:00:00', '2019-06-06 20:00:00', 1, 1, 6, 1, 40),
+       ('2019-06-07 11:00:00', '2019-06-07 20:00:00', 1, 0, 6, 1, 41);
+#        ('2019-06-05 11:00:00', '2019-06-05 20:00:00', 1, 1, 10, 5, 51),
+#        ('2019-06-06 11:00:00', '2019-06-06 20:00:00', 1, 1, 10, 5, 52),
+#        ('2019-06-07 11:00:00', '2019-06-07 20:00:00', 1, 0, 10, 5, 53);
 
 
 INSERT INTO reviews (title, body, is_recommended, babysitter_id, parent_id, appointment_id)
-VALUES ('Highly Recommended!', 'Everybody in the city should hire this babysitter. She\'s just that good.', 1, 7, 2, 4),
+VALUES ('Highly Recommended!', 'Everybody in the city should hire this babysitter. She\'s just that good.', 1, 6, 1, 1),
        ('Don\'t waste your money, people!', 'This babysitter spilled a bag of potato chips on our couch and didn\'t
-       even bother to clean up the mess. Horrid!', 0, 8, 3, 8),
+       even bother to clean up the mess. Horrid!', 0, 6, 1, 2),
+       ('Highly Recommended!', 'Everybody in the city should hire this babysitter. She\'s just that good.', 1, 6, 1, 3),
+       ('Don\'t waste your money, people!', 'This babysitter spilled a bag of potato chips on our couch and didn\'t
+       even bother to clean up the mess. Horrid!', 0, 7, 2, 4),
        ('Highly Recommended!', 'Everybody in the city should hire this babysitter. She\'s just that good.', 1, 7, 2, 5),
        ('Don\'t waste your money, people!', 'This babysitter spilled a bag of potato chips on our couch and didn\'t
        even bother to clean up the mess. Horrid!', 0, 7, 2, 6),
        ('Highly Recommended!', 'Everybody in the city should hire this babysitter. She\'s just that good.', 1, 8, 3, 7),
        ('Don\'t waste your money, people!', 'This babysitter spilled a bag of potato chips on our couch and didn\'t
-       even bother to clean up the mess. Horrid!', 0, 8, 3, 9),
-       ('Highly Recommended!', 'Everybody in the city should hire this babysitter. She\'s just that good.', 1, 9, 4, 10),
+       even bother to clean up the mess. Horrid!', 0, 8, 3, 8),
+       ('Highly Recommended!', 'Everybody in the city should hire this babysitter. She\'s just that good.', 1, 8, 3,
+        9),
        ('Don\'t waste your money, people!', 'This babysitter spilled a bag of potato chips on our couch and didn\'t
-       even bother to clean up the mess. Horrid!', 0, 9, 4, 11),
-       ('Highly Recommended!', 'Everybody in the city should hire this babysitter. She\'s just that good.', 1, 10, 5, 13),
-       ('Don\'t waste your money, people!', 'This babysitter spilled a bag of potato chips on our couch and didn\'t
-       even bother to clean up the mess. Horrid!', 0, 10, 5, 14);
-
+       even bother to clean up the mess. Horrid!', 0, 9, 4, 10);
 
 
 INSERT INTO specifications (is_smoker, has_cpr_training, has_transportation, years_of_experience, birthdate, babysitter_id)
-VALUES (true, true, true, 1, '1991-01-01', 6),
-       (true, true, true, 2, '1992-02-02', 7),
+VALUES (false, false, false, 1, '1991-01-01', 6),
+       (false, false, false, 2, '1992-02-02', 7),
        (false, false, false, 3, '1993-03-03', 8),
-       (false, true, false, 4, '1994-04-04', 9),
-       (true, false, true, 5, '1995-05-05', 10);
-
+       (false, false, false, 4, '1994-04-04', 9),
+       (false, false, false, 5, '1995-05-05', 10);
